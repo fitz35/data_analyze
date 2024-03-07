f474ea615106d2b5c5c7b33758cfa4afbd632c7f
fitz35 <clement@lahoche.info>
2024-03-07 16:06:53 +0100

    fix label size

--- src/params/mod.rs
+++ src/params/mod.rs
@@ -5,7 +5,7 @@
 /// size of one figure in the grid (width, height)
 pub const ONE_FIG_SIZE : (u32, u32) = (820, 640);
 
-pub const LABEL_HORIZONTAL_SIZE : u32 = 100;
+pub const LABEL_HORIZONTAL_SIZE : u32 = 150;
 
 pub const FIGURE_CAPTION_FONT_SIZE : u32 = 30;
 
